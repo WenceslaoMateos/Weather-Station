ba9055fc91adf93f7ed718620a4da8d4f2937795
wenceslaomateos <wenceslaomateos@gmail.com>
2022-12-11 13:40:52 -0300

    V2.0 preliminar

--- javascript.js
+++ javascript.js
@@ -8,21 +8,9 @@ const todayMaxMin = document.getElementById("today_max_min");
 const clock = document.getElementById("clock");
 const img_now = document.getElementById("img_now");
 
-let lat = "-38.0023";
-let lon = "-57.5575";
-
-// Global letiable with the json object with the weather data
+let lat;
+let lon;
 let weather_data;
-let ciudad;
-let days = [
-    "Domingo",
-    "Lunes",
-    "Martes",
-    "Miércoles",
-    "Jueves",
-    "Viernes",
-    "Sábado",
-];
 
 // Add days of the forecast week
 function add_days() {
@@ -34,7 +22,8 @@ function add_days() {
     for (let i = 0; i < date_unix.length; i++) {
         // Find the day of the week for the forecast weather
         let fecha = new Date(date_unix[i] * 1000);
-        let dayOfWeek = days[fecha.getDay()];
+        let dayOfWeek = fecha.toLocaleDateString("ES-ar", { weekday: "long" });
+        dayOfWeek = dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1);
         let sigFechaMili = weather_data.current.dt + 24 * 60 * 60 * (i + 1);
         let sigFecha = new Date(sigFechaMili * 1000);
         $("#day_plus_" + (i + 1)).html(dayOfWeek + " " + sigFecha.getDate());
@@ -88,31 +77,17 @@ function add_min_max() {
 // Fill the data of today weather
 function fill_today() {
     let options = {
-            timeZone: weather_data.timezone,
-            weekday: "long",
-        },
-        formatter = new Intl.DateTimeFormat("es-AR", options);
+        timeZone: weather_data.timezone,
+        weekday: "long",
+    };
+    let formatter = new Intl.DateTimeFormat("es-AR", options);
     let nombreDia = formatter.format(new Date());
 
     let date_unix = weather_data.current.dt;
     let fecha = new Date(date_unix * 1000);
 
-    let month = [
-        "Enero",
-        "Febrero",
-        "Marzo",
-        "Abril",
-        "Mayo",
-        "Junio",
-        "Julio",
-        "Agosto",
-        "Septiembre",
-        "Octubre",
-        "Noviembre",
-        "Diciembre",
-    ];
     let day_num = fecha.getDate();
-    let mes = month[fecha.getMonth()];
+    let mes = fecha.toLocaleDateString("ES-ar", { month: "long" });
     let temp = Math.round(weather_data.current.temp);
     todayTemp.innerText = `Hoy es ${nombreDia} ${day_num} de ${mes} y hacen ${temp} °C`;
 
@@ -128,38 +103,32 @@ function fill_today() {
 
 function showTime() {
     let options = {
-            timeZone: weather_data.timezone,
-            hour: "numeric",
-            minute: "numeric",
-        },
-        formatter = new Intl.DateTimeFormat("es-AR", options);
+        timeZone: weather_data.timezone,
+        hour: "numeric",
+        minute: "numeric",
+    };
+    let formatter = new Intl.DateTimeFormat("es-AR", options);
     let date = formatter.format(new Date());
     clock.innerText = date;
-    console.log("si");
 }
 
 // Fade the image that is selected
-wenElement.style["opacity"] = 0.5;
-susyElement.style["opacity"] = 1;
-juliElement.style["opacity"] = 0.5;
-hofiElement.style["opacity"] = 0.5;
-todayCity.innerText = `en Mar del Plata`;
-call_weather();
-
-wenElement.onclick = () => {
-    wenElement.style["opacity"] = 1;
-    susyElement.style["opacity"] = 0.5;
+function susyFunction() {
+    wenElement.style["opacity"] = 0.5;
+    susyElement.style["opacity"] = 1;
     juliElement.style["opacity"] = 0.5;
     hofiElement.style["opacity"] = 0.5;
     todayCity.innerText = `en Mar del Plata`;
     lat = -38.0023;
     lon = -57.5575;
     call_weather();
-};
+}
+susyFunction();
+susyElement.onclick = susyFunction;
 
-susyElement.onclick = () => {
-    wenElement.style["opacity"] = 0.5;
-    susyElement.style["opacity"] = 1;
+wenElement.onclick = () => {
+    wenElement.style["opacity"] = 1;
+    susyElement.style["opacity"] = 0.5;
     juliElement.style["opacity"] = 0.5;
     hofiElement.style["opacity"] = 0.5;
     todayCity.innerText = `en Mar del Plata`;
@@ -190,33 +159,28 @@ hofiElement.onclick = () => {
     call_weather();
 };
 
-let flag = 0;
-
-// This function obtain the JSON as letiable and call "appendData" to show it
 function call_weather() {
     fetch(
         `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=791da408c782eab70ae3c9336dcc4adc`
     )
         .then((response) => {
-            return response.text();
+            return response.json();
         })
         .then((data) => {
-            weather_data = JSON.parse(data);
+            weather_data = data;
+            showTime();
             add_days();
             add_icons();
             add_min_max();
             fill_today();
-            showTime();
-            if (flag === 0) {
-                setInterval(call_weather, 600000);
-                setInterval(showTime, 15000);
-                setInterval(add_days, 600000);
-                setInterval(add_icons, 600000);
-                setInterval(add_min_max, 600000);
-                setInterval(fill_today, 600000);
-                flag++;
-            }
         });
 }
 
 call_weather();
+
+setInterval(call_weather, 600000);
+setInterval(showTime, 15000);
+setInterval(add_days, 600000);
+setInterval(add_icons, 600000);
+setInterval(add_min_max, 600000);
+setInterval(fill_today, 600000);
